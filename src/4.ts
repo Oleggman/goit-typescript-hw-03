b31bd3e4aad176cc8f21316289edc952f0931309
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-11-23 00:45:59 +0200

    Done task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature (): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor (private key: Key) { }
+
+  getKey (): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor (protected key: Key) { }
+
+  comeIn (person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor (key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor (key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +47,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
